ba9a15d885877e0e59dfc790b428f6fbdad07027
Giadacantu <127940208+Giadacantu@users.noreply.github.com>
2023-05-23 15:41:27 +0200

    progetto vue riordinato

---
+++ src/App.vue
@@ -0,0 +1,74 @@
+<script>
+import HelloWorld from './components/HelloWorld.vue';
+import TheWelcome from './components/TheWelcome.vue';
+
+import Mycomponent from './components/Mycomponent.vue';
+
+import worksData from './assets/works.json';
+
+export default {
+  data() {
+    return {
+      works: worksData,
+    };
+  },
+  components: {
+    Mycomponent,
+  }
+};
+
+
+</script>
+
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      
+    </div>
+  </header>
+
+  <main>
+  
+  </main>
+
+    <div id="works">
+          <div v-for="work in works" :key="work.id">
+            <Mycomponent
+            :title="work.title"
+            :abstract="work.description"
+            :imageUrl="work.image_thumbnail"
+            />
+          </div>
+        </div>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
---
+++ src/components/Mycomponent.vue
@@ -0,0 +1,26 @@
+<script setup>
+defineProps({
+    title: String,
+    abstract: String,
+    imageUrl: String,
+})
+</script>
+
+<template>
+    <div class="section">
+        <div class="image">
+            <img :src="imageUrl" width="125" height="125" />
+        </div>
+        <div class="video-text">
+            <h1>{{ title }}</h1>
+            <p class="description">{{ abstract }}</p>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+img {
+    display: block;
+    border-radius: 100%;
+}
+</style>
